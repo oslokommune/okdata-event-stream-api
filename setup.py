a9de6a7d0aa2e68c9b18db0c33c69a8ff5b4ffb0
Petter H. Juliussen <petter.juliussen@byr.oslo.kommune.no>
2020-07-01 11:39:46 +0200

    DP-971: Add pydantic requirement

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.oslo.kommune.no/origo-dataplatform/event-streams-api",
     py_modules=["app"],
-    install_requires=["flask==1.1.2", "flask-restful==0.3.8"],
+    install_requires=["flask==1.1.2", "flask-restful==0.3.8", "pydantic==1.5.1"],
 )
