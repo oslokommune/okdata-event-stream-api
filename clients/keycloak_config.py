4703ccd972ff12c4785bea77652281795d40725e
Øyvind Nygard <oyvind.nygard@knowit.no>
2020-07-29 12:34:05 +0200

    DP-1018: Deploy application (#11)
    * DP-1018: Include python files and use correct iam policy
    
    * DP-1018: Use ssm.get_parameters instead of 3x get_parameter

--- clients/keycloak_config.py
+++ clients/keycloak_config.py
@@ -16,21 +16,22 @@ def get_keycloak_config():
 
     client_id = "event-stream-api"
 
-    client_secret = ssm_client.get_ssm_parameter(
-        f"/dataplatform/{client_id}/keycloak-client-secret", with_decryption=True,
-    )
+    client_secret_ssm_name = f"/dataplatform/{client_id}/keycloak-client-secret"
 
-    server_url = ssm_client.get_ssm_parameter(
-        "/dataplatform/shared/keycloak-server-url"
-    )
+    server_url_ssm_name = "/dataplatform/shared/keycloak-server-url"
 
-    realm_name = ssm_client.get_ssm_parameter("/dataplatform/shared/keycloak-realm")
+    realm_name_ssm_name = "/dataplatform/shared/keycloak-realm"
+
+    parameters = ssm_client.get_ssm_parameters(
+        [client_secret_ssm_name, server_url_ssm_name, realm_name_ssm_name],
+        with_decryption=True,
+    )
 
     return KeycloakConfig(
         client_id=client_id,
-        client_secret=client_secret,
-        server_url=server_url,
-        realm_name=realm_name,
+        client_secret=parameters[client_secret_ssm_name],
+        server_url=parameters[server_url_ssm_name],
+        realm_name=parameters[realm_name_ssm_name],
     )
 
 
@@ -38,8 +39,12 @@ class SSMClient:
     def __init__(self):
         self.client = boto3.client("ssm", region_name=os.environ["AWS_REGION"])
 
-    def get_ssm_parameter(self, parameter_name, with_decryption=False):
-        parameter = self.client.get_parameter(
-            Name=parameter_name, WithDecryption=with_decryption
-        )
-        return parameter["Parameter"]["Value"]
+    def get_ssm_parameters(self, parameter_names, with_decryption=False):
+        parameters = self.client.get_parameters(
+            Names=parameter_names, WithDecryption=with_decryption
+        )["Parameters"]
+        parameters_dict = {}
+        for parameter in parameters:
+            parameters_dict[parameter["Name"]] = parameter["Value"]
+
+        return parameters_dict
