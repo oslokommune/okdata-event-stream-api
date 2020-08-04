2acd79c4ce0262450ad262c66d7a4684c6bd1675
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-08-04 09:29:26 +0200

    DP-1021: Add ElasticDataService

---
+++ services/events.py
@@ -0,0 +1,25 @@
+import logging
+import json
+from database import ElasticsearchConnection
+from elasticsearch_dsl import Search
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class ElasticsearchDataService:
+    def get_event_by_date(self, dataset_id, version, from_date, to_date):
+        index = dataset_id + "-" + version + "-" + "*"
+        alias = "event_by_date"
+        es = ElasticsearchConnection()
+        es.connect_to_es(index, alias)
+        s = Search(using=alias, index=index).filter(
+            "range", tidspunkt={"gte": from_date, "lt": to_date}
+        )
+        response = s.execute()
+
+        if not response:
+            logger.warning("Could not get a response from ES")
+            return None
+        sourcelist = [e.to_dict() for e in s]
+        return json.dumps(sourcelist)
