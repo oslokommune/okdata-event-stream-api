a808910c6d9e33be64ae0269de8833325bc0a317
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-08-05 10:33:10 +0200

    DP-1022: Add count function

--- services/events.py
+++ services/events.py
@@ -20,3 +20,26 @@ class ElasticsearchDataService:
             logger.warning("Could not get a response from ES")
             return None
         return [e.to_dict() for e in s]
+
+    def get_event_count(self, dataset_id, version, from_date, to_date):
+        index = dataset_id + "-" + version + "-" + "*"
+        alias = "event_by_count"
+        es = ElasticsearchConnection()
+        es.connect_to_es(index, alias)
+        body = {
+            "size": 0,
+            "aggs": {
+                "count_events": {
+                    "range": {
+                        "field": "tidspunkt",
+                        "ranges": [{"from": from_date, "to": to_date}],
+                    }
+                }
+            },
+        }
+        s = Search(using=alias, index=index).update_from_dict(body)
+        response = s.execute()
+
+        count = response["aggregations"]["count_events"]["buckets"][0]["doc_count"]
+
+        return count
