3a380f209b9f3320c00192fd381020f76a6f1c4c
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-08-05 10:38:28 +0200

    DP-1022: Add auth to test get event

--- test/resources/events_resource_test.py
+++ test/resources/events_resource_test.py
@@ -1,4 +1,7 @@
+import pytest
 from services.events import ElasticsearchDataService
+
+from .conftest import valid_token
 
 event_dummy_data = {
     "tidspunkt": "2020-06-02T09:48:58+02:00",
@@ -8,39 +11,65 @@ event_dummy_data = {
     "stasjon": "haraldrud-gjenbruk",
 }
 dataset_id = "besoksdata"
-version = "gjenbruksstasjoner"
+version = "1"
 from_date = "2020-06-01"
 to_date = "2020-06-05"
-doc_count = 10899
+
+auth_header = {"Authorization": f"bearer {valid_token}"}
 
 
 class TestGetEvent:
-    def test_GetEventHistory(self, monkeypatch, mock_client):
-        def get_event_history_data(self, dataset_id, version, from_date, to_date):
-            return event_dummy_data
-
-        monkeypatch.setattr(
-            ElasticsearchDataService, "get_event_by_date", get_event_history_data,
-        )
+    def test_GetEventHistory(
+        self,
+        mock_client,
+        mock_event_data,
+        mock_dataset_versions,
+        mock_authorizer,
+        mock_keycloak,
+    ):
         response = mock_client.get(
-            "/besoksdata/gjenbruksstasjoner/events?from_date=2020-06-01&to_date=2020-06-05"
+            f"/{dataset_id}/{version}/events?from_date={from_date}&to_date={to_date}",
+            headers=auth_header,
         )
         response_data = response.get_json()
-        print(response_data)
         assert response.status_code == 200
         assert response_data["stasjonId"] == 41
 
-    def test_GetEventHistory_no_data(self, monkeypatch, mock_client):
-        def get_event_history_data(self, dataset_id, version, from_date, to_date):
-            return None
-
-        monkeypatch.setattr(
-            ElasticsearchDataService, "get_event_by_date", get_event_history_data,
-        )
+    def test_GetEventHistory_no_data(
+        self,
+        mock_client,
+        mock_event_no_data,
+        mock_dataset_versions,
+        mock_authorizer,
+        mock_keycloak,
+    ):
         response = mock_client.get(
-            "/besoksdata/Noneexisting/events?from_date=2020-06-01&to_date=2020-06-05"
+            f"/{dataset_id}/{version}/events?from_date={from_date}&to_date={to_date}",
+            headers=auth_header,
         )
         response_data = response.get_json()
-        print(response_data)
         assert response.status_code == 400
-        assert response_data["message"] == "No event found for provided id"
+        assert (
+            response_data["message"] == f"Could not found event: {dataset_id}/{version}"
+        )
+
+
+### Fixtures for TestGetEvent ###
+@pytest.fixture()
+def mock_event_data(monkeypatch):
+    def get_event_by_date(self, dataset_id, version, from_date, to_date):
+        return event_dummy_data
+
+    monkeypatch.setattr(
+        ElasticsearchDataService, "get_event_by_date", get_event_by_date,
+    )
+
+
+@pytest.fixture()
+def mock_event_no_data(monkeypatch):
+    def get_event_by_date(self, dataset_id, version, from_date, to_date):
+        return None
+
+    monkeypatch.setattr(
+        ElasticsearchDataService, "get_event_by_date", get_event_by_date,
+    )
