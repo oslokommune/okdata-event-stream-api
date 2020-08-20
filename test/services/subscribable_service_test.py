ae56ca8ce0974672ede43b849b07b8bc883a772d
Håvard Eide <havard.eide@byr.oslo.kommune.no>
2020-08-20 13:32:07 +0200

    DP-1058 - move tests for subscribable template to separate folder

--- test/services/subscribable_service_test.py
+++ test/services/subscribable_service_test.py
@@ -12,7 +12,6 @@ from services.exceptions import (
     ParentResourceNotReady,
     ResourceConflict,
 )
-from services.template import SubscribableTemplate
 
 from test import test_utils
 import test.test_data.subscribable as test_data
@@ -117,17 +116,6 @@ def test_disable_subscribable(mock_boto, mock_dataset):
         subscribable_service.disable_subscribable(
             test_data.dataset_id, test_data.version, test_data.updated_by
         )
-
-
-def test_generate_subscribable_cf_template():
-    dataset = {"Id": test_data.dataset_id, "confidentiality": test_data.confidentiality}
-    template = SubscribableTemplate(dataset, test_data.version)
-    subscribable_template = template.generate_stack_template()
-
-    test_utils.validate_cf_template(subscribable_template.json())
-    assert (
-        json.loads(subscribable_template.json()) == test_data.subscribable_cf_template
-    )
 
 
 @pytest.fixture()
