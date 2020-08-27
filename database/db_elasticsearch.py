d932fc892ae6e231bdd9125c01b19a8253f18a1d
Fredrik Vraalsen <fredrik@vraalsen.no>
2020-08-27 07:02:50 +0200

    DP-939 Add auth headers to Elasticsearch query
    Based on example in
    https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-request-signing.html#es-request-signing-python

--- database/db_elasticsearch.py
+++ database/db_elasticsearch.py
@@ -1,18 +1,42 @@
 import logging
 import os
 
+from elasticsearch import RequestsHttpConnection
 from elasticsearch_dsl import connections
+from requests_aws4auth import AWS4Auth
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # the Elasticsearch API endpoint
 path = os.environ["ES_API_ENDPOINT"]
+region = os.environ["AWS_REGION"]
 
 
 class ElasticsearchConnection:
     def connect_to_es(self, index, alias):
-        logger.warning("Connecting to ES")
-        connections.create_connection(
-            alias, hosts=[path], timeout=2,
-        )
+        logger.info(f"Connecting to ES: {path}")
+        try:
+            service = "es"
+            credentials = boto3.Session().get_credentials()
+            awsauth = AWS4Auth(
+                credentials.access_key,
+                credentials.secret_key,
+                region,
+                service,
+                session_token=credentials.token,
+            )
+
+            connections.create_connection(
+                alias,
+                hosts=[{"host": path, "scheme": "https", "port": 443}],
+                http_auth=awsauth,
+                use_ssl=True,
+                verify_certs=True,
+                connection_class=RequestsHttpConnection,
+                timeout=2,
+            )
+            logger.info("Connected to ES")
+        except Exception as e:
+            logger.error(f"Unable to connect to ES: {str(e)}")
